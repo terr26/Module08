1f1731325aba8e87854b38daa904f2a9f8cf01b1
terr26 <tongolterrenceliam@gmail.com>
2024-11-27 13:16:13 +0800

    Add project files.

---
+++ Services/UserService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Module08.Model;
+using System.Net.Http.Json;
+
+namespace Module08.Services
+{
+    public class UserService
+    {
+        private readonly HttpClient _httpClient;
+        private const string BaseUrl = "http://localhost/pdc50/";
+        public UserService()
+        {
+        _httpClient = new HttpClient();
+        }
+
+        //GetFromJsonAsync - method to call HTTP GET
+        //PostAsJsonAsync - method to call HTTP POST
+        //ReadAsStrinAsycn - metho to read the content of HTTPContent
+
+        //Get User
+        public async Task<List<User>> GetUsersAsync()
+        {
+            var response = await _httpClient.GetFromJsonAsync<List<User>>($"{BaseUrl}get_user.php");
+            return response ?? new List<User> ();
+        }
+
+        //Add User
+        public async Task<string>AddUserAsync(User user)
+        {
+            var response = await _httpClient.PostAsJsonAsync($"{BaseUrl}add_user.php", user);
+            var result = await response.Content.ReadAsStringAsync();
+            return result;
+        }
+
+        //Update User
+        public async Task<string> UpdateUserAsync(User user)
+        {
+            var response = await _httpClient.PostAsJsonAsync($"{BaseUrl}update_user.php", user);
+            var result = await response.Content.ReadAsStringAsync();
+            return result;
+        }
+
+        //Delete User
+        public async Task<string> DeleteUserAsync(int userId)
+        {
+            var response = await _httpClient.PostAsJsonAsync($"{BaseUrl}delete_user.php", new {id = userId});
+            var result = await response.Content.ReadAsStringAsync();
+            return result;
+        }
+    }
+}
---
+++ ViewModel/UserViewModel.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Module08.Model;
+using Module08.Services;
+using System.Windows.Input;
+
+namespace Module08.ViewModel
+{
+    public class UserViewModel : BindableObject
+    {
+        private readonly UserService _userService;
+        public ObservableCollection<User> Users { get; set; }
+        private User _selectedUser;
+        public User SelectedUser
+        {
+            get => _selectedUser;
+            set
+            {
+                _selectedUser = value;
+                OnPropertyChanged();
+                UpdateEntryField();
+            }
+        }
+
+        private string _nameInput;
+
+        //Name
+        public string NameInput
+        {
+            get => _nameInput;
+            set
+                {
+                _nameInput = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _genderInput;
+
+        //Gender
+        public string GenderInput
+        {
+            get => _genderInput;
+            set
+            {
+                _genderInput = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _contactnoInput;
+
+        //ContactNo
+        public string ContactNoInput
+        {
+            get => _contactnoInput;
+            set
+            {
+                _contactnoInput = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void ClearInput()
+        {
+            NameInput = string.Empty;
+            GenderInput = string.Empty;
+            ContactNoInput = string.Empty;
+        }
+
+        private void UpdateEntryField()
+        {
+            if(SelectedUser != null)
+            {
+                NameInput = SelectedUser.Name;
+                GenderInput = SelectedUser.Gender;
+                ContactNoInput = SelectedUser.ContactNo;
+            }
+        }
+
+        public UserViewModel()
+        {
+            _userService = new UserService();
+            Users = new ObservableCollection<User>();
+            LoadUserCommand = new Command(async () => await LoadUsers());
+            AddUserCommand = new Command(async () => await AddUser());
+            DeleteUserCommand = new Command(async () => await DeleteUser());
+            UpdateUserCommand = new Command(async () => await UpdateUser());
+
+        }
+
+        public ICommand LoadUserCommand { get; }
+        public ICommand AddUserCommand { get; }
+        public ICommand DeleteUserCommand { get; }
+
+        public ICommand UpdateUserCommand { get; }
+
+        private async Task UpdateUser()
+        {
+            if(SelectedUser != null)
+            {
+                SelectedUser.Name = NameInput;
+                SelectedUser.Gender = GenderInput;
+                SelectedUser.ContactNo = ContactNoInput;
+
+                var result = await _userService.UpdateUserAsync(SelectedUser);
+                await LoadUsers();
+            }
+        }
+
+        private async Task DeleteUser()
+        {
+            if(SelectedUser != null)
+            {
+                var result = await _userService.DeleteUserAsync(SelectedUser.ID);
+                await LoadUsers();
+            }
+        }
+        private async Task LoadUsers()
+        {
+            var users = await _userService.GetUsersAsync();
+            Users.Clear();
+            foreach (var user in users)
+            {
+                Users.Add(user);
+            }
+        }
+
+        private async Task AddUser()
+        {
+            if(!string.IsNullOrWhiteSpace(NameInput)&&
+                !string.IsNullOrWhiteSpace(GenderInput)&&
+                !string.IsNullOrWhiteSpace(ContactNoInput))
+            {
+                var newUser = new User
+                {
+                    Name = NameInput,
+                    Gender = GenderInput,
+                    ContactNo = ContactNoInput
+                };
+                var result = await _userService.AddUserAsync(newUser);
+
+                if (result.Equals("Success", StringComparison.OrdinalIgnoreCase))
+                {
+                    await LoadUsers();
+                }
+            }
+        }
+    }
+}
